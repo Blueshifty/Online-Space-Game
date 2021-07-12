6c6efbc2bd528e4988833b4bdee673c140f4b037
Mehmet Bir <blueshift.zoso@gmail.com>
2021-07-12 15:53:38 +0300

    Missin Files Added

---
+++ ZombieGame.Business/DTOs/Response/RoomOnDashboardDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ZombieGame.Business.DTOs.Response
+{
+    public class RoomOnDashboardDto
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int PlayerCount { get; set; }
+    }
+}
